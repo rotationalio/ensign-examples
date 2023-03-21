c8277e2ae600976824f6b15d6b7a60725da7bffc
Rebecca Bilbro <bilbro@gmail.com>
2023-03-21 10:27:36 -0400

    update minimal example with API changes

--- go/minimal/main.go
+++ go/minimal/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/oklog/ulid"
 	ensign "github.com/rotationalio/go-ensign"
 	api "github.com/rotationalio/go-ensign/api/v1beta1"
 	mimetype "github.com/rotationalio/go-ensign/mimetype/v1beta1"
@@ -18,39 +19,74 @@ type MessageInABottle struct {
 	Timestamp string `json:"timestamp,omitempty"`
 }
 
+const CocoaBeans = "chocolate-covered-espresso-beans"
+
 func main() {
+	// Create Ensign Client
 	client, err := ensign.New(&ensign.Options{
 		ClientID:     os.Getenv("ENSIGN_CLIENT_ID"),
 		ClientSecret: os.Getenv("ENSIGN_CLIENT_SECRET"),
+		// AuthURL:      "https://auth.ensign.world", // uncomment if you are in staging
+		// Endpoint:     "staging.ensign.world:443", // uncomment if you are in staging
 	})
 	if err != nil {
 		panic(fmt.Errorf("could not create client: %s", err))
 	}
 
+	// Create Ensign Publisher
 	pub, err := client.Publish(context.Background())
 	if err != nil {
 		panic(fmt.Errorf("could not create publisher: %s", err))
 	}
 
+	// Check to see if topic exists and create it if not
+	exists, err := client.TopicExists(context.Background(), CocoaBeans)
+	if err != nil {
+		panic(fmt.Errorf("unable to check topic existence: %s", err))
+	}
+
+	var topicID string
+	if !exists {
+		if topicID, err = client.CreateTopic(context.Background(), CocoaBeans); err != nil {
+			panic(fmt.Errorf("unable to create topic: %s", err))
+		}
+	} else {
+		topics, err := client.ListTopics(context.Background())
+		if err != nil {
+			panic(fmt.Errorf("unable to retrieve project topics: %s", err))
+		}
+
+		for _, topic := range topics {
+			if topic.Name == CocoaBeans {
+				var topicULID ulid.ULID
+				if err = topicULID.UnmarshalBinary(topic.Id); err != nil {
+					panic(fmt.Errorf("unable to retrieve requested topic: %s", err))
+				}
+				topicID = topicULID.String()
+			}
+		}
+	}
+
+	// Prep event data and marshal for transmission
 	data := MessageInABottle{
 		Sender:    "Enson the Sea Otter",
 		Timestamp: time.Now().String(),
 		Message:   "You're looking smart today!",
 	}
-
 	e := &api.Event{
-		TopicId:  "chocolate-covered-espresso-beans",
 		Mimetype: mimetype.ApplicationJSON,
 		Type: &api.Type{
 			Name:    "Generic",
 			Version: 1,
 		},
 	}
-
 	e.Data, _ = json.Marshal(data)
-	pub.Publish(e)
 
-	sub, err := client.Subscribe(context.Background())
+	// Publish event
+	pub.Publish(topicID, e)
+
+	// Create a downstream consumer for the event stream
+	sub, err := client.Subscribe(context.Background(), topicID)
 	if err != nil {
 		panic(fmt.Errorf("could not create subscriber: %s", err))
 	}
