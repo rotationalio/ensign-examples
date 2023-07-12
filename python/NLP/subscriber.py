1eeacf0e04a54ea2b3c52a46fe4307300f27da50
Rebecca Bilbro <bilbro@gmail.com>
2023-07-12 17:42:28 -0400

    cleanup

--- python/NLP/subscriber.py
+++ python/NLP/subscriber.py
@@ -4,8 +4,8 @@ import msgpack
 import warnings
 
 from pyensign.ensign import Ensign
-from pyensign.api.v1beta1.ensign_pb2 import Nack  
- 
+from pyensign.api.v1beta1.ensign_pb2 import Nack
+
 from bs4 import BeautifulSoup
 from textblob import TextBlob
 import spacy
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")
 
 class BaleenSubscriber:
     """
-    Implementing an event-driven Natural Language Processing tool that 
+    Implementing an event-driven Natural Language Processing tool that
     does streaming HTML parsing, entity extraction, and sentiment analysis
     """
     def __init__(self, topic="documents"):
@@ -25,36 +25,33 @@ class BaleenSubscriber:
         Initilaize the BaleenSubscriber, which will allow a data consumer
         to subscribe to the topic that the publisher is pushing articles
         """
-        
+
         self.topic = topic
-        self.ensign = Ensign(
-            client_id = 'TwggMcedZxmrbBLPgoNSFfRSioTZEPhk',
-            client_secret = 'K5l9i4HLz4kmiyFNkelfYoA5pzGBBcVnFzJtdtBWZAwnsyIhWH5uGqGxsL7duB9c'
-            )
+        self.ensign = Ensign()
         self.NER = spacy.load('en_core_web_sm')
-    
+
     def run(self):
         """
         Run the subscriber forever.
         """
         asyncio.get_event_loop().run_until_complete(self.subscribe())
-    
+
     async def handle_event(self,event):
         """
         Decode and ack the event.
-    
+
         ----------------
-        Unpacking of the event message and working on the article content for 
+        Unpacking of the event message and working on the article content for
         NLP Magic
         """
-        
+
         try:
             data = msgpack.unpackb(event.data)
         except json.JSONDecodeError:
             print("Received invalid JSON in event payload:", event.data)
             await event.nack(Nack.Code.UNKNOWN_TYPE)
             return
-    
+
         # Parsing the content using BeautifulSoup
         soup = BeautifulSoup(data['content'], 'html.parser')
         # Finding all the 'p' tags in the parsed content
@@ -71,10 +68,10 @@ class BaleenSubscriber:
                         ner_dict[word.label_].append(word.text)
                 else :
                     ner_dict[word.label_] = [word.text]
-                     
+
         print("\nSentiment Average Score : ", sum(score) / len(score))
         print("\n------------------------------\n")
-        print("Named Entities : \n",json.dumps( 
+        print("Named Entities : \n",json.dumps(
                 ner_dict,
                 sort_keys=True,
                 indent=4,
@@ -82,7 +79,7 @@ class BaleenSubscriber:
                 )
               )
         await event.ack()
-    
+
     async def subscribe(self):
        """
        Subscribe to the article and parse the events.
