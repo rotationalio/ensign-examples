dcc4738e6ff23ee31c3f616777b56ba1d974bdb2
Patrick Deziel <42919891+pdeziel@users.noreply.github.com>
2024-02-13 17:29:56 -0600

    Diy llm 3 (#26)

---
+++ courses/diy_llm/module_3_getting_into_production/app.py
@@ -0,0 +1,31 @@
+import asyncio
+import streamlit as st
+from pyensign.ensign import Ensign
+from pyensign.ml.dataframe import DataFrame
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+
+def handle_input(sent):
+    st.text_area("Enter a movie review", key="input")
+    if st.button("Predict Sentiment"):
+        input_text = st.session_state.input
+        result = sent(input_text)
+        st.write("Sentiment:", result[0]["label"])
+        st.write("Confidence:", result[0]["score"])
+
+async def app(ensign):
+    st.title("Movie Review Sentiment Analysis")
+
+    # Read the latest model from Ensign + Hugging Face
+    query = "SELECT * FROM sentiment-models"
+    cursor = await ensign.query(query)
+    models = await DataFrame.from_events(cursor)
+    model_path = models.iloc[-1]["model_path"]
+    model_version = models.iloc[-1]["model_version"]
+    st.write("Using model {} @ {}".format(model_path, model_version))
+    model = AutoModelForSequenceClassification.from_pretrained(model_path, revision=model_version)
+    tokenizer = AutoTokenizer.from_pretrained(model_path, revision=model_version)
+    sent = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    handle_input(sent)
+
+if __name__ == "__main__":
+    asyncio.run(app(Ensign())),
